a667307b8ebe751c4ba14a202918ed75020131af
Artem Zadorov <artemzadorov@Artems-Laptop.local>
2024-05-03 16:29:38 +0300

    auth

--- internal/michman/app/middleware/encryptor.go
+++ internal/michman/app/middleware/encryptor.go
@@ -7,30 +7,30 @@ import (
 )
 
 func Encryptor(secret, authPath string) func(next http.Handler) http.Handler {
+
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if secret != "" {
 				hashHeader := r.Header.Get("jwt")
-				if hashHeader != "" {
-					if err := auth(hashHeader, authPath); err != nil {
-						http.Error(w, err.Error(), http.StatusForbidden)
-						return
-					}
+
+				if !auth(hashHeader, authPath) {
+					http.Error(w, "", http.StatusForbidden)
+					return
 				}
+
 			}
 			next.ServeHTTP(w, r)
 		})
 	}
 }
 
-func auth(header, authPath string) error {
+func auth(header, authPath string) bool {
 	absPath := fmt.Sprintf("http://" + authPath + "/token/view/")
 	client := http.Client{}
 
 	req, err := http.NewRequest("POST", absPath, nil)
 	if err != nil {
-		// log.Print("I CANT REQUEST")
-		return err
+		return false
 	}
 
 	req.Header.Add("jwt", header)
@@ -39,13 +39,15 @@ func auth(header, authPath string) error {
 	response, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
-		return err
+		return false
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode == 200 {
-		return nil
+		// log.Print(response.StatusCode)
+		return true
 	} else {
-		return err
+		log.Print(response.StatusCode)
+		return false
 	}
 }
