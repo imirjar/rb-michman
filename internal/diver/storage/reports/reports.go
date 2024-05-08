eb5ae62d57d1b43f2d79319bd9ed3d3ec1f0529b
Artem Zadorov <artemzadorov@Artems-Laptop.local>
2024-05-08 14:49:04 +0300

    fix divers store, add reports db to /db folder

--- internal/diver/storage/reports/reports.go
+++ internal/diver/storage/reports/reports.go
@@ -4,54 +4,37 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"time"
 
 	"github.com/imirjar/Michman/internal/diver/models"
 	_ "modernc.org/sqlite"
 )
 
 type ReportsStore struct {
-	db *sql.DB
+	dbConn *sql.Conn
 }
 
 func NewReportStore() *ReportsStore {
-	db, err := sql.Open("sqlite", "reports")
+	db, err := sql.Open("sqlite", "db/reports")
 	if err != nil {
 		panic(err)
 	}
 
-	repStore := ReportsStore{
-		db: db,
-	}
-
-	if err := repStore.Ping(); err != nil {
+	conn, err := db.Conn(context.Background())
+	if err != nil {
 		panic(err)
 	}
 
-	return &repStore
-}
-
-func (r *ReportsStore) Ping() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-	return r.db.PingContext(ctx)
+	return &ReportsStore{
+		dbConn: conn,
+	}
 }
 
 func (r *ReportsStore) GetQuery(ctx context.Context, id string) (string, error) {
-	// log.Print(id)
-
-	conn, err := r.db.Conn(ctx)
-	if err != nil {
-		return err.Error(), err
-	}
-	defer conn.Close()
 
 	var data string
 
-	//id is an argument !!!
-	// q := db.QueryRow()
-	row := conn.QueryRowContext(ctx, "SELECT query FROM reports WHERE id=$1;", id)
-	err = row.Scan(&data)
+	row := r.dbConn.QueryRowContext(ctx, "SELECT query FROM reports WHERE id=$1;", id)
+	err := row.Scan(&data)
 	if err != nil {
 		return err.Error(), err
 	}
@@ -60,13 +43,8 @@ func (r *ReportsStore) GetQuery(ctx context.Context, id string) (string, error) 
 }
 
 func (r *ReportsStore) GetAllReports(ctx context.Context) (string, error) {
-	conn, err := r.db.Conn(ctx)
-	if err != nil {
-		return err.Error(), err
-	}
-	defer conn.Close()
 
-	rows, err := conn.QueryContext(ctx, "SELECT * FROM reports;")
+	rows, err := r.dbConn.QueryContext(ctx, "SELECT * FROM reports;")
 	if err != nil {
 		return err.Error(), err
 	}
